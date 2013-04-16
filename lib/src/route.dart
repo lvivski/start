0aa908499e6a1208875b9c8f6c4e067dc92850fd
Yehor Lvivski <lvivski@gmail.com>
2013-04-16 13:48:10 +0300

    router fix, closes #9

--- lib/src/route.dart
+++ lib/src/route.dart
@@ -8,21 +8,21 @@ import 'response.dart';
 import 'socket.dart';
 
 class Route {
-  String _method;
+  String _method, _dir;
   Map _path;
   Function _action;
-  String _dir;
   StreamController _controller;
   
-  Route(this._method, path, this._action) {
-    this._path = _normalize(path);
+  Route(String method, path, this._action) {
+    _method = method.toUpperCase();
+    _path = _normalize(path);
   }
   
   Route.file(this._dir); 
   
   Route.ws(path, action) {
-    this._method = 'ws';
-    this._path = _normalize(path);
+    _method = 'WS';
+    _path = _normalize(path);
     _controller = new StreamController();
     _controller.stream.transform(new WebSocketTransformer()).listen((WebSocket ws){
       var socket = new Socket(ws);
@@ -31,14 +31,14 @@ class Route {
   }
   
   match(HttpRequest req) {
-    return ((_method.toLowerCase() == req.method.toLowerCase())
+    return ((_method == req.method || _method == 'WS')
         && _path['regexp'].hasMatch(req.uri.path));
   }
   
   handle(HttpRequest req, [view]) {
     if (_dir != null) {
       new Response(req.response).sendFile(_dir + req.uri.path);
-    } else if (_controller != null) {
+    } else if (_method == 'WS') {
       _controller.add(req);
     } else {
       var request = new Request(req);
@@ -57,27 +57,28 @@ class Route {
     if (path is List) {
       path = '(${path.join('|')})';
     }
-    path = path.concat(strict ? '' : '/?');
-
-    List keys = [];
-
-    RegExp placeholderRE = new RegExp(r'(\.)?:(\w+)(\?)?');
-    Iterable<Match> matches = placeholderRE.allMatches(path);
-
-    for (Match placeholder in matches) {
-      StringBuffer replace = new StringBuffer('(?:');
-      if (placeholder[1] != null) {
-        replace.write('\.');
-      }
-      replace.write('(\\w+))');
-      if (placeholder[3] != null) {
-        replace.write('?');
-      }
-      path = path.replaceFirst(placeholder[0], replace.toString());
-      keys.add(placeholder[2]);
-    }
-
-    path = path.replaceAll('//', '/');
+    
+    var keys = [];
+    
+    path = path.concat(strict ? '' : '/?')
+        .replaceAllMapped(new RegExp(r'(\.)?:(\w+)(\?)?'), (Match placeholder) {
+          var replace = new StringBuffer('(?:');
+          
+          if (placeholder[1] != null) {
+            replace.write('\.');
+          }
+          
+          replace.write('(\\w+))');
+          
+          if (placeholder[3] != null) {
+            replace.write('?');
+          }
+          
+          keys.add(placeholder[2]);
+          
+          return replace.toString();
+        })
+        .replaceAll('//', '/');
 
     return {
       'regexp': new RegExp('^$path\$'),
@@ -86,7 +87,7 @@ class Route {
   }
 
   Map _parseParams(String path, Map routePath) {
-    Map params = {};
+    var params = {};
     Match paramsMatch = routePath['regexp'].firstMatch(path);
     for (var i = 0; i < routePath['keys'].length; i++) {
       params[routePath['keys'][i]] = paramsMatch[i+1];
