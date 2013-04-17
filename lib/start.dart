bfb2378e20786c82ced11a7fbf73fc12d1e62dbd
Yehor Lvivski <lvivski@gmail.com>
2013-04-17 18:49:34 +0300

    better types

--- lib/start.dart
+++ lib/start.dart
@@ -1,7 +1,9 @@
 library start;
 
+import 'dart:async' show Future;
+
 import 'server.dart';
 
-start({view, String public: 'public', String host: '127.0.0.1', int port: 80}) {
+Future<Server> start({view, String public: 'public', String host: '127.0.0.1', int port: 80}) {
   return new Server(view, public).listen(host, port);
 }
