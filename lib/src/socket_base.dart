8a20d6441cf11a307c91fd800d79fedcf5e981c4
Yehor Lvivski <lvivski@gmail.com>
2013-11-04 00:19:37 +0200

    optimize imports

--- lib/src/socket_base.dart
+++ lib/src/socket_base.dart
@@ -2,12 +2,10 @@ library start_socket;
 
 import 'dart:async';
 
-import 'message.dart';
-
 export 'message.dart' show Message;
 
 abstract class SocketBase {
-  void send(String msg_name, { data });
+  void send(String msg_name, [ data ]);
 
   Stream on(String message_name);
 
