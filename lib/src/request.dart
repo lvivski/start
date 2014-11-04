fc0320e881c4c0568b014a0b0af4cf7088aa444b
Hoylen Sue <hoylen@hoylen.com>
2014-11-04 20:33:15 +1000

    Fixed param method to return POST request parameters.

--- lib/src/request.dart
+++ lib/src/request.dart
@@ -35,6 +35,17 @@ class Request {
 
   X509Certificate get certificate => _request.certificate;
 
+  /// Returns the value of the HTTP parameter with the given _name_.
+  ///
+  /// Returns the value of the named parameter from the query string or the
+  /// request body, or an empty string if the it is not defined.
+  ///
+  /// If the parameter is defined in both, the value from the request body is returned.
+  ///
+  /// Note: when processing POST requests, the [payload] method should be
+  /// called beforehand. Otherwise any parameters set in the request body
+  /// are ignored.
+
   String param(String name) {
     var value;
     value = params[name];
@@ -48,10 +59,18 @@ class Request {
     return ''; // no parameter with name found
   }
 
+  /// Parses the request body for query parameters.
+  ///
+  /// Used for handling POST requests. This method must be used
+  /// before [param] will return parameters from the request body,
+  /// otherwise it only examines the query string.
+  ///
+  /// Returns the parameters parsed as a [Map] of names and values.
+
   Future<Map> payload({ Encoding enc: UTF8 }) {
     var completer = new Completer();
     _request.transform(const AsciiDecoder()).listen((content) {
-      final params = new Map.fromIterable(
+      params = new Map.fromIterable(
           content.split('&').map((kvs) => kvs.split('=')),
           key: (kv) => Uri.decodeQueryComponent(kv[0], encoding: enc),
           value: (kv) => Uri.decodeQueryComponent(kv[1], encoding: enc)
