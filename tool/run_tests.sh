466c9f5e5e3953e4938731cc3e93599027dc4eef
Rodrigo Muniz <rodrigo@muniz>
2019-07-15 23:54:02 -0300

    Removed checked flag from test script

--- tool/run_tests.sh
+++ tool/run_tests.sh
@@ -6,6 +6,6 @@ DIR=$( cd $( dirname "${BASH_SOURCE[0]}" )/.. && pwd )
 
 echo "Analyzing library for warnings or type errors"
 dartanalyzer --show-package-warnings $DIR/lib/start.dart
-dart --checked $DIR/test/start_test.dart
+dart $DIR/test/start_test.dart
 
 echo -e "\n[32m✓ OK[0m"
